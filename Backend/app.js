3eeb4d2d0a3b9ecaf9670af9acc385c01db582a8
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-08-27 22:32:47 +0530

    Initial commit and setup of Project

---
+++ Backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:PORT`);
+});
