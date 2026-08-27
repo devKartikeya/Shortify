6c8aa88a561ae6cd271083f96ca715a6982094fc
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-08-27 23:33:22 +0530

    Add User model

---
+++ Backend/users/users.model.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+
+        password: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
