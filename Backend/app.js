035ef42574a702b703902ef035ded659f63c6f12
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-09-09 21:39:20 +0530

    Add tests to Backend

--- Backend/app.js
+++ Backend/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const cors = require("cors");
 const connectDB = require('./configurations/database');
 const cookieParser = require("cookie-parser");
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 const userRouter = require("./users/users.route");
 const urlRouter = require("./urls/urls.route");
+
 const {
     redirectUrlController
 } = require("./urls/urls.controller");
+
 
 app.use(
   cors({
@@ -21,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-connectDB();
+// connectDB();
 
 app.use("/users", userRouter);
 app.use("/urls", urlRouter);
@@ -35,6 +38,4 @@ app.get('/', (req, res) => {
   res.send('Hello from Express backend!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:PORT`);
-});
+module.exports = app;
