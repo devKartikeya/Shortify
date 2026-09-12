40964e0d416e084d5c47f23f228ec65e58d23c43
Kartikeya Mishra <kartikeya2122008@gmail.com>
2026-09-12 23:28:03 +0530

    Add connection of Frontend to Load Balancer

--- Backend/app.js
+++ Backend/app.js
@@ -15,27 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/redis-test", async (req, res) => {
-  await redisClient.set("name", "Kartikeya");
-
-  const name = await redisClient.get("name");
-
-  res.json({
-    message: "Redis is working!",
-    name
-  });
-});
-
 app.use("/users", userRouter);
 app.use("/urls", urlRouter);
 
-app.get(
-  "/:shortCode",
-  redirectUrlController
-);
+app.get("/:shortCode", redirectUrlController);
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express backend 2!');
+  res.send('Hello from Express backend!');
 });
 
 
